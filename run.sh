0f109243bd8096e8e972265f37674c8a9f70cf41
jyx0416 <yuxinji728@gmail.com>
2026-02-06 17:46:55 +0800

    移除不必要的环境变量设置

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # export PYTHONPATH=/home/jetson/python_ws/arx5-sdk/python:$PYTHONPATH
-export OLLAMA_FLASH_ATTENTION=1
-export OLLAMA_KV_CACHE_TYPE=q8_0
 export PYTHONPATH=/home/jyx/python_ws/arx5-sdk/python:$PYTHONPATH
 python run_realtime.py "$@"
